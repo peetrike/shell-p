e6ea6a7fc0d4018259ec28f2f702046744ec3e13
Meelis Nigols <meelis.nigols@telia.ee>
2019-11-08 21:54:21 +0200

    changed formatting

--- shell/ch08/Solutions/echoargs-2.sh
+++ shell/ch08/Solutions/echoargs-2.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 #echoargs
 
-  # option l default value
+    # option l default value
 opt_l=1
 
-  # process options
+    # process options
 while getopts ":l" opt ; do
-  case $opt in
-    l) opt_l=0  # option l specified on command line
-       ;;
-    *) echo "Usage: $0 [-l] args..."
-       exit 1
-       ;;
-  esac
+    case $opt in
+        l) opt_l=0  # option l specified on command line
+        ;;
+        *) echo "Usage: $0 [-l] args..."
+            exit 1
+        ;;
+    esac
 done
 
-  # remove processed parameters
+    # remove processed parameters
 shift $((OPTIND-1))
 
-  # do the stuff
+    # do the stuff
 if [ $opt_l -eq 0 ] ; then
-    # put every parameter on separate line
-  for word # in "$@"
-  do
-    echo $word
-  done
+        # put every parameter on separate line
+    for word # in "$@"
+    do
+        echo $word
+    done
 else
-    # put all parameters on same line
-  echo $*
+        # put all parameters on same line
+    echo $*
 fi
