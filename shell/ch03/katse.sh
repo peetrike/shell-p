b5b689cb4b28e926533b18e20a31918aa82be077
Meelis Nigols <meelis.nigols@telia.ee>
2019-11-12 22:27:03 +0200

    remvoed schapter 2 examples

--- shell/ch03/katse.sh
+++ shell/ch03/katse.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
-# see kript teeb midagi olulist
+# see skript teeb midagi olulist
 
 ls
     # see on kommentaar
 date
 pwd # rea lõpu kommentaar
-
-# output/error redirect
-    # töötab
-rm bozzo > minu.txt 2>&1
-    # ei tööta
-rm bozzo > minu.txt 2&>1
-
-    # viskame väljundi ära
-echo "Tere" > /dev/null
-rm bozzo 2> /dev/null
