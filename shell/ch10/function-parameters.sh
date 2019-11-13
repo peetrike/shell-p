a20228e286e4489de9dd7abee4baf05928fc7a8a
Meelis Nigols <meelis.nigols@telia.ee>
2019-11-13 11:16:36 +0200

    added local variable to function

--- shell/ch10/function-parameters.sh
+++ shell/ch10/function-parameters.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 shifter() {
+    local ajutine
     echo "  Funktsioon Shifter alustas"
     echo "meil on $# parameetrit"
+    ajutine=$1
     shift
     echo "nüüd on $# parameetrit"
     echo $1
+    echo "kadunud parameeter: $ajutine"
 }
 
 echo "skriptis on $# parameetrit"
